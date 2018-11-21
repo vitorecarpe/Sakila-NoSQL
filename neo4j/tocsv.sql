d13dcef910f641987455a0ba781d88fa695c2894
MLobo1997 <miguelobo1997@gmail.com>
2018-11-21 11:50:22 +0000

    added staff without relation to store and about to add stores

--- neo4j/tocsv.sql
+++ neo4j/tocsv.sql
@@ -27,7 +27,27 @@ FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n';
 
 
-select * from customer
-into outfile '/usr/local/Cellar/neo4j/3.4.5/libexec/import/customers.csv'
+--Staff null uniformization
+update staff
+set picture = ''
+where picture is null;
+
+update staff
+set password = ''
+where password is null;
+
+--exportação de staff (sem store para já). Tem de ser por esta ordem por causa das dependencias
+select first_name, last_name, address, city, email, active, username, password from staff
+inner join address on staff.address_id = address.address_id
+inner join city on address.city_id = city.city_id
+into outfile '/usr/local/Cellar/neo4j/3.4.5/libexec/import/staff.csv'
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n';
+
+select address, city, username as manager from store
+inner join staff on store.manager_staff_id = staff.staff_id
+inner join address on store.address_id = address.address_id
+inner join city on address.city_id = city.city_id
+into outfile '/usr/local/Cellar/neo4j/3.4.5/libexec/import/stores.csv'
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n';
